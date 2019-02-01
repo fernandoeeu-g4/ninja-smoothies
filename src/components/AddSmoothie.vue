0c49f321dcf413e46340a2486decba6d8c5e01a7
Fernando Antonio <fernando.acouto@outlook.com>
2019-01-31 22:57:22 -0300

    31012019

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -6,11 +6,16 @@
         <label for="title">Smoothied Title:</label>
         <input type="text" name="title" v-model="title">
       </div>
+      <div v-for="(ing, index) in ingredients" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]">
+      </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient</label>
-        <input @keydown.tab="addIng" v-model="another" type="text" name="add-ingredient">
+        <input @keydown.tab.prevent="addIng" v-model="another" type="text" name="add-ingredient">
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -18,27 +23,49 @@
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
-  name: 'AddSmoothie',
+  name: "AddSmoothie",
   data() {
     return {
       title: null,
       another: null,
+      feedback: null,
+      slug: "",
       ingredients: []
-    }
+    };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title)
+      if (!this.title) {
+        this.feedback = "You must add a title";
+      } else if (this.ingredients.length < 1) {
+        this.feedback = "You must add at least one ingredient";
+      } else {
+        this.feedback = null;
+        this.generateSlug();
+        console.log(this.slug);
+        // db.collections("smoothie").add({
+        //   ingredients: this.ingredients,
+        //   slug: this.sl
+        // });
+      }
     },
     addIng() {
       if (this.another) {
-        this.ingredients.push(this.another)
-        console.log(this.ingredients)
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value to add an in";
       }
+    },
+    generateSlug() {
+      this.slug = str.toLowerCase(str.replace(" ", "-"));
     }
   }
-}
+};
 </script>
 
 <style>
